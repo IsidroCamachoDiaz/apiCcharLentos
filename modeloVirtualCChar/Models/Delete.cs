52454b3efcd70e9784c143adeb5f6dd308e53ce0
ISIDRO\isidr <isidroswitch@gmail.com>
2023-11-01 18:09:17 +0100

    1-11-2023

---
+++ modeloVirtualCChar/Models/Delete.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class Delete
+{
+    public gestorBibliotecaContext gb;
+
+    public Delete(gestorBibliotecaContext gestorBibliotecaContext)
+    {
+        this.gb = gestorBibliotecaContext;
+    }
+    public void borrarAutor(Autor autor)
+    {
+        this.gb.delete(autor);
+    }
+    public void borrarRelacionAutoresLibros(Rel_Autores_Libros relacion)
+    {
+        this.gb.delete(relacion);
+    }
+    public void borrarLibro(Libro libro)
+    {
+        this.gb.delete(libro);
+    }
+    public void borrarEditorial(Editorial editorial)
+    {
+        this.gb.delete(editorial);
+    }
+    public void borrarGenero(Genero genero)
+    {
+        this.gb.delete(genero);
+    }
+    public void borrarColeccion(Coleccion coleccion)
+    {
+        this.gb.delete(coleccion);
+    }
+    public void borrarAcceso(Acceso acceso)
+    {
+        this.gb.delete(acceso);
+    }
+    public void borrarUsuario(Usuario usuario)
+    {
+        this.gb.delete(usuario);
+    }
+    public void borrarPrestamo(Prestamo prestamo)
+    {
+        this.gb.delete(prestamo);
+    }
+    public void borrarEstadoPrestamo(Estado_Prestamo estadoPrestamo)
+    {
+        this.gb.delete(estadoPrestamo);
+    }
+    public void borrarRelacionPrestamoLibro(Rel_Prestamo_Libro relacion)
+    {
+        this.gb.delete(relacion);
+    }
+
+}
---
+++ modeloVirtualCChar/Models/Insert.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Insert
+{
+    public gestorBibliotecaContext gb;
+
+    public Insert(gestorBibliotecaContext gestorBibliotecaContext)
+    {
+        this.gb=gestorBibliotecaContext;
+    }
+
+    public void insertarAutor(Autor a)
+    {
+        this.gb.insert(a);
+    }
+    public void insertarRelacionAutoresLibros(Rel_Autores_Libros relacion)
+    {
+        this.gb.insert(relacion);
+    }
+    public void insertarLibro(Libro libro)
+    {
+        this.gb.insert(libro);
+    }
+    public void insertarEditorial(Editorial editorial)
+    {
+        this.gb.insert(editorial);
+    }
+    public void insertarGenero(Genero genero)
+    {
+        this.gb.insert(genero);
+    }
+    public void insertarColeccion(Coleccion coleccion)
+    {
+        this.gb.insert(coleccion);
+    }
+    public void insertarAcceso(Acceso acceso)
+    {
+        this.gb.insert(acceso);
+    }
+    public void insertarUsuario(Usuario usuario)
+    {
+        this.gb.insert(usuario);
+    }
+    public void insertarPrestamo(Prestamo prestamo)
+    {
+        this.gb.insert(prestamo);
+    }
+    public void insertarEstadoPrestamo(Estado_Prestamo estadoPrestamo)
+    {
+        this.gb.insert(estadoPrestamo);
+    }
+    public void insertarRelacionPrestamoLibro(Rel_Prestamo_Libro relacion)
+    {
+        this.gb.insert(relacion);
+    }
+
+
+
+}
